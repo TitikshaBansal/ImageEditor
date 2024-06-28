51de99db128ef601fb1be8a1a20ceef62bc9b648
Titiksha Bansal <titikshabansal0209@gmail.com>
2024-06-28 19:09:57 +0530

    Filter Image function completed in detail

--- Image.cpp
+++ Image.cpp
@@ -4,10 +4,55 @@
 using namespace cv;
 using namespace std;
 
+// functions for more specific tasks.
+void Gray(Mat& address){
+    cvtColor(address, address, COLOR_BGR2GRAY);
+    imshow("Grayscale Image", address);
+    waitKey(0);
+}
+void Blur(Mat& address){
+    // Take input for kernel size (blur intensity)
+    int kernelSize;
+    cout << "Enter the kernel size (odd value): ";
+    cin >> kernelSize;
+
+    // Apply Gaussian blur
+    GaussianBlur(address, address, Size(kernelSize, kernelSize), 0);
+
+    // Display the blurred image
+    imshow("Blurred Image", address);
+    waitKey(0);
+}
+void Sharp(Mat& address){
+    double sharpenFactor;
+    std::cout << "Enter sharpening factor (0.0 to 1.0): ";
+    std::cin >> sharpenFactor;
+
+    // Gaussian blur
+    Mat blurred;
+    GaussianBlur(address, blurred, Size(0, 0), 25);
+
+    addWeighted(address, 1.0 + sharpenFactor, blurred, -sharpenFactor, 0, address);
+
+    // Display results
+    imshow("Sharpened", address);
+    waitKey(0);
+}
+void AdjustColor(Mat& address){
+    
+}
+void Brightness(Mat& address){
+    
+}
+void Contrast(Mat& address){
+    
+}
+
 // functions to perform tasks
 Mat loadImage(){
     string str;
     cout<< "Enter the path to  your image.\n";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, str);
     Mat img = imread(str, IMREAD_COLOR);
 
@@ -25,10 +70,56 @@ Mat loadImage(){
     return img;
 }
 void FilterImage(Mat& address){
-    
+    cout<<"\n \n Choose one filter option from the following to be applied:\n";
+    cout<<"1. Gray Scale\n";
+    cout<<"2. Blur\n";
+    cout<<"3. Sharpen\n";
+
+    int choice;
+    cin>> choice;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+
+    //Calling functions according to the choice made.
+    switch (choice){
+        case 1:
+            Gray(address);
+            break;
+        case 2:
+            Blur(address);
+            break;
+        case 3:
+            Sharp(address);
+            break;
+        default:
+            cout<<"Enter a valid choice.\n";
+            break;
+        }   
 }
 void colorImage(Mat& address){
-    
+    cout<<"\n \n Choose one color adjusting option from the following to be applied:\n";
+    cout<<"1. Colors\n";
+    cout<<"2. Brightness\n";
+    cout<<"3. Contrast\n";
+
+    int choice;
+    cin>> choice;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+
+    //Calling functions according to the choice made.
+    switch (choice){
+        case 1:
+            AdjustColor(address);
+            break;
+        case 2:
+            Brightness(address);
+            break;
+        case 3:
+            Contrast(address);
+            break;
+        default:
+            cout<<"Enter a valid choice.\n";
+            break;
+        }
 }
 void cropImage(Mat& address){
     int startX, startY, width, height;
@@ -36,9 +127,9 @@ void cropImage(Mat& address){
     cin >> startX;
     cout << "Enter the starting Y-coordinate: ";
     cin >> startY;
-    cout << "Enter the width of the ROI: ";
+    cout << "Enter the width of the ROI (Region of Interest): ";
     cin >> width;
-    cout << "Enter the height of the ROI: ";
+    cout << "Enter the height of the ROI (Region of Interest): ";
     cin >> height;
 
     // Crop the image
@@ -57,8 +148,8 @@ void resizeImage(Mat& address){
 void saveImage(Mat& address){
     cout<< "Enter the location where you want your edited image to be saved. \n example: path/to/save/MyImage.jpg\n ";
     string str;
-    getline(cin,str);
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,str);
 
     bool check = imwrite(str, address);
 
